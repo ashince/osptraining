9c9ac18a079f9cc3a0190b61e8d8e68d1fe3cd15
ashince <ashincekd@gmail.com>
2017-12-27 23:50:35 +0530

    6
    6

--- help.sh
+++ help.sh
@@ -1,7 +1,6 @@
 ## check storage iptables service and entry in /etc/sysconfig/iptables file
 
 cd ~
-git clone https://github.com/ashince/osptraining.git
 cp /home/stack/osptraining/instackenv.json .
 cp /home/stack/osptraining/storage-environment.yaml /home/stack/templates/storage-environment.yaml
 cp /home/stack/osptraining/compute.yaml /home/stack/templates/nic-configs/compute.yaml
