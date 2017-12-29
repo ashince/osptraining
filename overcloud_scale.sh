508d983745d0534e92902605123f94d845f8a3d7
ashince <ashincekd@gmail.com>
2017-12-29 14:51:35 +0530

    Create overcloud_scale.sh

---
+++ overcloud_scale.sh
@@ -0,0 +1,8 @@
+openstack overcloud deploy --templates \
+-e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+-e /home/stack/templates/network-environment.yaml \
+-e /home/stack/templates/storage-environment.yaml \
+--control-flavor control --compute-flavor compute \
+--control-scale 3 --compute-scale 2 \
+--ntp-server pool.ntp.org --neutron-network-type vxlan \
+--neutron-tunnel-types vxlan
